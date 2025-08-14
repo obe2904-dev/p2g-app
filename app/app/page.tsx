c257082c00ad46a0972fa17e489a52c15d419163
obe2904-dev <obe2904@gmail.com>
2025-08-14 19:51:37 +0200

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-      <h2>Velkommen</h2>
-      <p>Denne side er kun til test af MVP. Når vi har bygget resten, kommer login og posts.</p>
-    </main>
-  )
-}
