7aae045ab7c5d31b7a5ce804e4ce907befaecebe
obe2904-dev <obe2904@gmail.com>
2025-08-14 19:53:38 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h2>Velkommen</h2>
+      <p>Denne side er kun til test af MVP. Når vi har bygget resten, kommer login og posts.</p>
+    </main>
+  )
+}
