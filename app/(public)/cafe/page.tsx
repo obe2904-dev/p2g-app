4801a4bffb0f5d9aae18bfcdb160deeddc08bd8c
obe2904-dev <obe2904@gmail.com>
2025-08-17 18:50:37 +0200

    Create page.tsx

---
+++ app/(public)/cafe/page.tsx
@@ -0,0 +1,9 @@
+export default function CafeLanding() {
+  return (
+    <main style={{ maxWidth: 720, margin: '40px auto' }}>
+      <h1>Post2Grow til caféer</h1>
+      <p>Dette er en midlertidig landingsside for /cafe. (Indhold kommer senere.)</p>
+      <p><a href="/auth?mode=signup">Opret gratis konto</a></p>
+    </main>
+  );
+}
