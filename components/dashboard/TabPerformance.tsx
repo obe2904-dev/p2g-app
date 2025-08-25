f192055dfa3aa889a3b7a41352cbc0396c3a571a
obe2904-dev <obe2904@gmail.com>
2025-08-25 22:51:07 +0200

    Create TabPerformance.tsx

---
+++ components/dashboard/TabPerformance.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Card from './Card';
+
+export default function TabPerformance() {
+  return (
+    <Card title="Performance" style={{ minHeight: 180 }}>
+      (Kommer snart) – Topopslag / tider / kanaler.
+    </Card>
+  );
+}
