0fb314337ce294c2824dcd7c9164d359460dfc51
obe2904-dev <obe2904@gmail.com>
2025-08-25 22:50:30 +0200

    Create TabPlanning.tsx

---
+++ components/dashboard/TabPlanning.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Card from './Card';
+
+export default function TabPlanning() {
+  return (
+    <Card title="Planlæg & udgiv" style={{ minHeight: 180 }}>
+      (Kommer snart) – Kalender/planlægning.
+    </Card>
+  );
+}
